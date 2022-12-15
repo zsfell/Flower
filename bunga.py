bd33c72d181b17a91a864bf01612bb2407282f67
just call me irsan <115108493+zsfell@users.noreply.github.com>
2022-12-15 10:28:04 +0700

    Create bunga.py

---
+++ bunga.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+title("lovv uu bbyyy")
+speed(0)
+bgcolor("black")
+r,g,b=255,0,0
+
+for i in range(500):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    circle(225-i,90)
+    lt(90)
+    circle(225-i,90)
+    left(18)
+    pencolor(r,g,b)
